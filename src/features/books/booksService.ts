0bbadeb731e5c654a89435d1779c9ed8c506568d
Yu Qin <sirius.qinyu@gmail.com>
2022-07-26 12:18:14 +0800

    change category api

--- src/features/books/booksService.ts
+++ src/features/books/booksService.ts
@@ -1,36 +1,12 @@
 import { splitApi } from "../../store/query/splitApi";
-import type {
-    BaseCategory,
-    GetCategoriyesResponse,
-    TopCategory,
-} from "./booksTypes";
-
-export function transformCategoryRes(
-    data: GetCategoriyesResponse
-): TopCategory[] {
-    const categories = data.categories;
-
-    const result = categories.map((topCategory) => {
-        const subCategories: BaseCategory[][] = [];
-
-        for (const metaType of topCategory.subCategorySequence) {
-            subCategories.push(topCategory.subCategories[metaType]);
-        }
-        return {
-            key: topCategory.key,
-            displayName: topCategory.displayName,
-            subCategories,
-        };
-    });
-
-    return result;
-}
+import type { TopCategory } from "./booksTypes";
 
 export const booksApi = splitApi.injectEndpoints({
     endpoints: (build) => ({
         getCategoriyes: build.query<TopCategory[], void>({
             query: () => "categories",
-            transformResponse: transformCategoryRes,
+            transformResponse: (res: { categories: TopCategory[] }) =>
+                res.categories,
         }),
     }),
 });
--- src/features/books/booksTypes.ts
+++ src/features/books/booksTypes.ts
@@ -1,29 +1,8 @@
-/**
- * category api return type, same as backend api
- */
 export interface BaseCategory {
     key: string;
     displayName: string;
 }
 
-type BaseCategoryArray = BaseCategory[];
-
-type metaType = string;
-
-type SubCategoryOutput = { [key: metaType]: BaseCategoryArray };
-
-export interface TopCategoryOutput extends BaseCategory {
-    subCategorySequence: metaType[];
-    subCategories: SubCategoryOutput;
-}
-
-export interface GetCategoriyesResponse {
-    categories: TopCategoryOutput[];
-}
-
-/**
- *  category types in store
- */
 export interface TopCategory extends BaseCategory {
     subCategories: BaseCategory[][];
 }
