80c751db9ff6f642faa262510ea2eb1b3ee21f10
Yu Qin <sirius.qinyu@gmail.com>
2022-08-28 18:10:02 +0800

    Temporary Login Button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import BookList from '../features/books/components/BookList'
 import CategoryNav from '../features/books/components/Category'
+import TestWeChatLogin from '../features/user/TestWeChatLogin'
 
 export default function home() {
     return (
         <>
+            <TestWeChatLogin />
+
             <CategoryNav />
             <BookList />
         </>
