ec907b978fb558819c7c73ddce7b22957ca4f265
Yu Qin <sirius.qinyu@gmail.com>
2022-11-15 15:28:47 +0800

    Add transcription

--- src/features/questionSet/components/Transcription.tsx
+++ src/features/questionSet/components/Transcription.tsx
@@ -3,6 +3,14 @@ export default function Transcription({
 }: {
     transcription?: string
 }) {
-    if (!transcription) return <></>
-    return <div>{transcription}</div>
+    return (
+        <div>
+            <div className="mb-2 font-bold text-green-600">听力原文</div>
+            {transcription ? (
+                <div style={{ whiteSpace: 'pre-wrap' }}>{transcription}</div>
+            ) : (
+                <div>无听力原文</div>
+            )}
+        </div>
+    )
 }
